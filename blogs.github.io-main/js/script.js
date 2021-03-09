3bc793d80d08a697709a91224eb7cbd64db07a25
Amit Kumar <52822539+STUDIOUS-WOLF@users.noreply.github.com>
2021-03-10 02:09:26 +0530

    Add files via upload
    Made nav bar responsive
    Added about page

---
+++ blogs.github.io-main/js/script.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', 
+{
+	pagination: 
+	{
+		el: '.swiper-pagination',
+		    dynamicBullets: true,
+	},
+	autoplay: 
+	{
+		delay: 3400,
+		speed: 300,
+	},
+});
+
+ function myfunction()
+ {
+	 var x=getElementById("navbar")
+ }
